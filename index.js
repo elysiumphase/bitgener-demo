c4559c77df597ac08c336f463886dfd3d6de8fd3
Adrien Valcke <adrienvalcke@icloud.com>
2020-09-23 13:26:38 +0200

    remove google sitemap and link to console

--- index.js
+++ index.js
@@ -66,28 +66,6 @@ const httpServer = http.createServer(async (request, response) => {
       response.statusCode = 500;
       response.end(`unable to load favicon: ${err.message}`);
     }
-  } else if (request.method === 'GET' && request.url === '/googlee0d81878ea8f20d1.html') {
-    try {
-      const google = getFileStream('googlee0d81878ea8f20d1.html', 'utf-8');
-      response.setHeader('Content-Type', 'text/html');
-      response.statusCode = 200;
-      await pipeline(google, response);
-    } catch (err) {
-      console.error(err);
-      response.statusCode = 500;
-      response.end(`unable to load google site verification page: ${err.message}`);
-    }
-  } else if (request.method === 'GET' && request.url.startsWith('/sitemap.xml')) {
-    try {
-      const sitemap = getFileStream('sitemap.xml', 'utf-8');
-      response.setHeader('Content-Type', 'application/xml');
-      response.statusCode = 200;
-      await pipeline(sitemap, response);
-    } catch (err) {
-      console.error(err);
-      response.statusCode = 500;
-      response.end(`unable to load sitemap file: ${err.message}`);
-    }
   } else {
     response.statusCode = 404;
     response.end('not found');
